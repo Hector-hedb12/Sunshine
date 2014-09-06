0b44c5b51dfc7f03dfd9c8bda4fe332d783b344a
Hector <hedb_12@hotmail.com>
2014-09-06 18:26:15 -0430

    Data Base Contract

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -7,7 +7,7 @@ import android.provider.BaseColumns;
  */
 public class WeatherContract {
 
-    public static final class WeatherEntry implements BaseColumns{
+    public static final class WeatherEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "weather";
 
@@ -23,7 +23,7 @@ public class WeatherContract {
         public static final String COLUMN_DEGRESS    = "degress";
     }
 
-    public static final class LocationEntry implements BaseColumns{
+    public static final class LocationEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "location";
 
